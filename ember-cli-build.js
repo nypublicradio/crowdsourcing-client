1406b69ce4f31b961ed4d8fc1d9fef9f27d75c0c
Brian Whitton <me@brianwhitton.com>
2017-10-19 11:27:12 -0400

    merge normalize css into vendor tree

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,11 +1,20 @@
 /* eslint-env node */
 'use strict';
+const Funnel = require('broccoli-funnel');
+const mergeTrees = require('broccoli-merge-trees');
+const path = require('path');
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
+  let normalizeTree = new Funnel(path.dirname(require.resolve('normalize.css')), {
+    files: ['normalize.css']
+  });
+
   let app = new EmberApp(defaults, {
-    // Add options here
+    trees: {
+      vendor: mergeTrees(['vendor', normalizeTree])
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -21,5 +30,6 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('vendor/normalize.css');
   return app.toTree();
 };
