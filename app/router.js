f336fccfb7615890fb015d6e394127d29459afe4
Brian Whitton <me@brianwhitton.com>
2017-10-19 18:48:59 -0400

    adds survey and survey/step route

--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('survey', {path: ':id'}, function() {
+    this.route('step', {path: ':step'});
+  });
 });
 
 export default Router;
---
+++ app/routes/survey/step.js
@@ -0,0 +1,16 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  model({ step }) {
+    return {
+      step,
+      survey: this.modelFor('survey')
+    };
+  },
+  setupController(controller, { survey }) {
+    this._super(...arguments);
+    if (survey.get('hasAudioQuestion')) {
+      controller.set('surveyType', 'audio');
+    }
+  }
+});
