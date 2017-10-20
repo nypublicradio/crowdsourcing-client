171f2219cc9188195b496304849b2df98e49cbf2
Brian Whitton <me@brianwhitton.com>
2017-10-20 00:08:59 -0400

    move this logic to the template
    each component needs different inputs and this was the clearest way I
    could come up with to make sure the args were explicit enough to
    understand
    
    this uses the progress service, which is injected into the audio survey
    manager (and potentially other "survey manager" components), to pass
    certain args, in addition any args provided by the route.
    
    also the `next` action is passed in, whcih is controlled by the survey
    manager but provided args by the components themselves.

--- app/components/audio-survey-manager/component.js
+++ app/components/audio-survey-manager/component.js
@@ -1,19 +1,8 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default Component.extend({
   router: service(),
-  which: computed('step', function() {
-    switch(this.get('step')) {
-    case '1':
-      return 'audio-recorder';
-    case '2':
-      return 'recording-playback';
-    case '3':
-      return 'personal-info';
-    }
-  }),
   next() {
     let step = this.incrementProperty('step');
     this.get('router').transitionTo('survey.step', this.get('survey'), step);
