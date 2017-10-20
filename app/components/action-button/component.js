d94cf961d79dd45c307416b2af849187c412ac37
Brian Whitton <me@brianwhitton.com>
2017-10-20 15:24:44 -0400

    bind disabled attribute on action-button

--- app/components/action-button/component.js
+++ app/components/action-button/component.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  tagName: 'button',
-  classNames: ['action-button']
+  tagName:           'button',
+  classNames:        ['action-button'],
+  attributeBindings: ['disabled']
 });
