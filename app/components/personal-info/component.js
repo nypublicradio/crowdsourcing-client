730ae1bb8a0de936de150e4249e1a2feb3d1d7ed
Brian Whitton <me@brianwhitton.com>
2017-10-20 15:26:16 -0400

    basic personal info logic

--- app/components/personal-info/component.js
+++ app/components/personal-info/component.js
@@ -1,4 +1,13 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  tagName: 'form',
+  classNames: ['personal-info'],
+  
+  submit(e) {
+    e.preventDefault();
+    if (this.get('onSubmit')) {
+      this.get('onSubmit')();
+    }
+  }
 });
