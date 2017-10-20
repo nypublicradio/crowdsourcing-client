c80ba079b48756f6b47208e5a3ce9d39aa5cbb80
Brian Whitton <me@brianwhitton.com>
2017-10-20 00:15:20 -0400

    refactor audio-recorder to just call the passed in `next` method

--- app/components/audio-recorder/component.js
+++ app/components/audio-recorder/component.js
@@ -2,18 +2,16 @@ import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 
 export default Component.extend({
-  router: service(),
-  twilio: service(),
+  twilio:  service(),
   
   toggleRecord() {
     let twilio = this.get('twilio');
     if (twilio.get('record.isIdle')) {
       twilio.get('record').perform();
     } else {
-      let connection = twilio.get('currentConnection');
       twilio.disconnect();
-      twilio.getRecordingURL(connection.parameters.CallSid)
-        .then(() => this.get('next')());
+      let connection = twilio.get('currentConnection');
+      this.get('next')({ key: 'connection', value: connection });
     }
   }
 });
