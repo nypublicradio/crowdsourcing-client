bf6ca60b7f97fe043e26a30f71aeac6eda541461
Brian Whitton <me@brianwhitton.com>
2017-10-20 14:43:24 -0400

    white space

--- mirage/factories/survey.js
+++ mirage/factories/survey.js
@@ -1,8 +1,8 @@
 import { Factory, faker } from 'ember-cli-mirage';
 
 export default Factory.extend({
-  title: () => `Survey: ${faker.random.words(3)}`,
-  summary: faker.lorem.paragraph,
+  title:    () => `Survey: ${faker.random.words(3)}`,
+  summary:  () => `Summary: ${faker.lorem.paragraph()}`,
   thankYou: faker.lorem.sentence,
   startsAt: faker.date.recent,
 });
