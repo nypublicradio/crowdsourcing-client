fcadd6dfa25b0d919be140f7da7852fef7ba52d5
Brian Whitton <me@brianwhitton.com>
2017-10-20 15:25:22 -0400

    this was rendering as a period when passed the function

--- mirage/factories/survey.js
+++ mirage/factories/survey.js
@@ -3,6 +3,6 @@ import { Factory, faker } from 'ember-cli-mirage';
 export default Factory.extend({
   title:    () => `Survey: ${faker.random.words(3)}`,
   summary:  () => `Summary: ${faker.lorem.paragraph()}`,
-  thankYou: faker.lorem.sentence,
+  thankYou: () => faker.lorem.sentence(),
   startsAt: faker.date.recent,
 });
