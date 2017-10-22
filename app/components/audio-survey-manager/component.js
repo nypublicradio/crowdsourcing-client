7de398fbf2fb5e80a738bc41b7b0b2f4d3078109
Brian Whitton <me@brianwhitton.com>
2017-10-22 13:33:01 -0500

    adds back button and text to survey manager header

--- app/components/audio-survey-manager/component.js
+++ app/components/audio-survey-manager/component.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   progress: service(),
@@ -7,13 +8,33 @@ export default Component.extend({
   
   classNames: ['audio-survey-manager'],
   
+  backText:   computed('step', function() {
+    switch(this.get('step')) {
+      case '2':
+      return 'Re-Record';
+      case '3':
+      return 'Cancel';
+      default:
+      return null;
+    }
+  }),
+  
   next({ key, value }) {
     let step = parseInt(this.get('step'), 10);
     this.get('progress.cache').set(key, value);
     this.get('router').transitionTo('survey.step', step + 1);
   },
   
+  back() {
+    let step = parseInt(this.get('step'), 10);
+    this.get('router').transitionTo('survey.step', step - 1);
+  },
+  
   finish() {
     this.get('router').transitionTo('survey.thank-you');
+  },
+  
+  cancel() {
+    this.get('router').transitionTo('survey.cancel');
   }
 });
