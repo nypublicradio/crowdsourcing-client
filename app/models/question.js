c466b8f0b3aa5d6ed01541080503b1ce542dd00c
Brian Whitton <me@brianwhitton.com>
2017-10-26 16:41:54 -0400

    adds question model constants for use in validator

--- app/models/question.js
+++ app/models/question.js
@@ -1,5 +1,17 @@
 import DS from 'ember-data';
 
+const TEXT     = 't';
+const EMAIL    = 'e';
+const AUDIO    = 'a'
+const TEXTAREA = 'x';
+
+export const TYPES = {
+  TEXT,
+  EMAIL,
+  AUDIO,
+  TEXTAREA,
+};
+
 export default DS.Model.extend({
   label:        DS.attr('string'),
   required:     DS.attr('boolean'),
