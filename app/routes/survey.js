211fb238bf7ec51fc80b3a9c50bc4c5dd444ce55
Brian Whitton <me@brianwhitton.com>
2017-10-26 07:42:12 -0400

    create a submission in the survey parent route
    and prep it with answer keys according to the submission

--- app/routes/survey.js
+++ app/routes/survey.js
@@ -1,11 +1,17 @@
 import Route from '@ember/routing/route';
-import rsvp from 'rsvp';
 
 export default Route.extend({
   model({ id }) {
-    return rsvp.hash({
-      survey: this.store.findRecord('survey', id),
-      submission: this.store.createRecord('submission')
-    });
+    return this.store.findRecord('survey', id)
+      .then(survey => {
+        let submission = this.store.createRecord('submission', { survey });
+        let answers = {};
+        survey.get('questions').mapBy('shortName').forEach(k => answers[k] = '');
+        submission.set('answers', answers);
+        return {
+          survey,
+          submission
+        };
+      });
   }
 });
