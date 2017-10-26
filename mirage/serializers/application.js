d6d24d4f1a4936536b391d4d9be5c91a51b88cae
Brian Whitton <me@brianwhitton.com>
2017-10-26 17:22:21 -0400

    application serializer for mirage

--- mirage/serializers/application.js
+++ mirage/serializers/application.js
@@ -1,4 +1,6 @@
 import { JSONAPISerializer } from 'ember-cli-mirage';
+import { singularize } from 'ember-inflector';
 
 export default JSONAPISerializer.extend({
+  typeKeyForModel: model => singularize(model.modelName)
 });
