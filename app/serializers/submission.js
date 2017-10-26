57bcf8d57cb5a155d79200d9079e747e510fe686
Brian Whitton <me@brianwhitton.com>
2017-10-26 17:19:43 -0400

    submission serializer
    format outgoing submission response to match server expectation: an
    array of objects with a question key (the question's ID) and a response
    key

---
+++ app/serializers/submission.js
@@ -0,0 +1,22 @@
+import DS from 'ember-data';
+import { singularize } from 'ember-inflector';
+
+export default DS.JSONAPISerializer.extend({
+  payloadKeyFromModelName: modelName => singularize(modelName),
+  serialize(snapshot/*, options*/) {
+    let payload = this._super(...arguments);
+    let questions = snapshot.record.get('survey.questions');
+    let { answers } = payload.data.attributes;
+    payload.data.attributes.answers = [];
+
+    questions.forEach(question => {
+      let { shortName, id } = question.getProperties('shortName', 'id');
+      payload.data.attributes.answers.push({
+        question: Number(id),
+        response: answers[shortName]
+      });
+    });
+    
+    return payload;
+  }
+});
