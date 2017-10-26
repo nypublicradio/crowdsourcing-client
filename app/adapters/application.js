dbf7b3e359481415ca9d2be5c3951f53981c67c6
Brian Whitton <me@brianwhitton.com>
2017-10-26 17:21:03 -0400

    crowdsourcing microservice has trailing slashes

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -5,4 +5,7 @@ import config from '../config/environment';
 export default DS.JSONAPIAdapter.extend({
   host: config.crowdsourcingService,
   pathForType: modelName => singularize(modelName),
+  buildURL() {
+    return this._super(...arguments) + '/';
+  }
 });
