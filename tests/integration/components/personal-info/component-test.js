5c3b7c1186ebe2bef4d50323132ab4c8eacb1007
Brian Whitton <me@brianwhitton.com>
2017-10-29 17:53:42 -0400

    personal info tests

--- tests/integration/components/personal-info/component-test.js
+++ tests/integration/components/personal-info/component-test.js
@@ -1,5 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import { fillIn, click } from 'ember-native-dom-helpers';
 
 moduleForComponent('personal-info', 'Integration | Component | personal info', {
   integration: true
@@ -14,8 +15,56 @@ test('it renders', function(assert) {
   assert.equal(this.$('.personal-info').length, 1);
 });
 
-test('renders expected questions');
+test('renders expected questions', function(assert) {
+  let questions = [{
+    shortName: 'first-name',
+  }, {
+    shortName: 'cats-name'
+  }];
+  let submission = { answers: {} };
+  this.setProperties({ questions, submission });
+  this.render(hbs`{{personal-info questions=questions submission=submission}}`);
+  
+  assert.equal(this.$('input.personal-info__input').length, 1, 'should only render the first-name input');
+});
 
-test('submitting executes the changeset and calls onSubmit');
+test('submitting executes the changeset and calls onSubmit', function(assert) {
+  assert.expect(2);
+  
+  let done = assert.async();
+  let questions = [{
+    shortName: 'first-name',
+  }, {
+    shortName: 'last-name'
+  }, {
+    shortName: 'email'
+  }];
+  let submission = { answers: {} };
+
+  this.setProperties({ questions, submission });
+  this.set('onSubmit', function() {
+    assert.ok('onSubmit called');
+  });
+
+  this.render(hbs`{{personal-info
+                    onSubmit=onSubmit
+                    questions=questions
+                    submission=submission}}`);
+
+  fillIn('[name=first-name]', 'foo').then(() => {
+    fillIn('[name=last-name]', 'bar').then(() => {
+      fillIn('[name=email]', 'buz@baz.com').then(() => {
+        click('.personal-info__submit').then(() => {
+          assert.deepEqual(submission.answers, {
+            'first-name': 'foo',
+            'last-name': 'bar',
+            'email': 'buz@baz.com'
+          });
+          done();
+        })
+      });
+    });
+  });
+});
 
 test('error messages');
