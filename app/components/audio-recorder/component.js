588bc65e038a0657560dc50960421361cd468b79
Brian Whitton <me@brianwhitton.com>
2017-10-29 17:06:58 -0400

    make this function a little safer and more testable
    only call `next` if it's defined
    use ember's `get` so we can use a POJO stub in testing

--- app/components/audio-recorder/component.js
+++ app/components/audio-recorder/component.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { get } from "@ember/object";
 
 export default Component.extend({
   classNames: ['audio-recorder'],
@@ -8,12 +9,15 @@ export default Component.extend({
   
   toggleRecord() {
     let twilio = this.get('twilio');
-    if (twilio.get('record.isIdle')) {
-      twilio.get('record').perform();
+    if (get(twilio, 'record.isIdle')) {
+      get(twilio, 'record').perform();
     } else {
       twilio.disconnect();
-      let connection = twilio.get('currentConnection');
-      this.get('next')({ key: 'connection', value: connection });
+      let connection = get(twilio, 'currentConnection');
+      let next = this.get('next');
+      if (next) {
+        next({ key: 'connection', value: connection });
+      }
     }
   }
 });
