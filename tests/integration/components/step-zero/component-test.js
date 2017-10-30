26634ba58ac31ab937c3a263801deb3dd43c68b7
Brian Whitton <me@brianwhitton.com>
2017-10-30 17:16:22 -0400

    step-zero tests

--- tests/integration/components/step-zero/component-test.js
+++ tests/integration/components/step-zero/component-test.js
@@ -1,12 +1,36 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import { find, click } from 'ember-native-dom-helpers';
 
 moduleForComponent('step-zero', 'Integration | Component | step zero', {
   integration: true
 });
 
 test('it renders', function(assert) {
-  this.render(hbs`{{step-zero}}`);
+  let title = 'Foo Title'; 
+  let summary = 'Bar Summary';
+  let survey = {
+    title,
+    summary
+  };
+  this.set('survey', survey);
+  this.render(hbs`{{step-zero survey=survey}}`);
 
-  assert.equal(this.$('.step-zero').length, 1);
+  assert.ok(find('.step-zero'));
+  assert.equal(find('.step-zero__title').textContent, title);
+  assert.equal(find('.step-zero__summary').textContent.trim(), summary);
 });
+
+test('transitions to step 1', function(assert) {
+  let router = {
+    transitionTo(route, arg) { 
+      assert.ok('transitionTo called');
+      assert.equal(route, 'survey.step');
+      assert.equal(arg, 1);
+    }
+  };
+  this.set('router', router);
+  
+  this.render(hbs`{{step-zero router=router}}`);
+  click('.action-button');
+})
