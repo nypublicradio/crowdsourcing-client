1c62323528c2b3d3f6569b23b902bc1b28b5b91e
Brian Whitton <me@brianwhitton.com>
2017-10-30 15:12:53 -0400

    manually update the record button UI based on a minimum duration
    can't stop recording until a minimum amount of audio has been processed;
    this keeps the button disabled until 2s past the connnection event

--- app/components/audio-recorder/component.js
+++ app/components/audio-recorder/component.js
@@ -1,15 +1,24 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
-import { get } from "@ember/object";
+import { get } from '@ember/object';
+import { later, bind } from '@ember/runloop';
 
 export default Component.extend({
-  classNames: ['audio-recorder'],
+  recordTimeout: 2000,
+  classNames:    ['audio-recorder'],
   
   twilio:  service(),
   
+  connectCallback() {
+    later(this, () => this.set('disableButton', false), get(this, 'recordTimeout'));  
+  },
+  
   toggleRecord() {
     let twilio = this.get('twilio');
+
     if (get(twilio, 'record.isIdle')) {
+      this.set('disableButton', true);
+      twilio.one('twilio-connected', bind(this, 'connectCallback'));
       get(twilio, 'record').perform();
     } else {
       twilio.disconnect();
