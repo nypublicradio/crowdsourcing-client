7f940b71ca2e09aa2aa8c01a3a1941246bef6ece
Brian Whitton <me@brianwhitton.com>
2017-10-31 12:54:38 -0400

    whitespace

--- app/models/submission.js
+++ app/models/submission.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   survey:  DS.belongsTo('survey'),
-  answers: DS.attr({defaultValue: () => ({})})
+  answers: DS.attr({ defaultValue: () => ({}) })
 });
