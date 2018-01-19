108240ee42453126b7b08ab272275c0a35f29ee5
Brian Whitton <me@brianwhitton.com>
2018-01-18 22:29:43 -0500

    dummhy expired route test

---
+++ tests/unit/routes/survey/expired-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:survey/expired', 'Unit | Route | survey/expired', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
