fb5dcd64e1d70799abb2ba7c26259fb7453fc82f
Brian Whitton <me@brianwhitton.com>
2018-01-23 14:55:52 -0500

    adds boilerplate for dynamic page titles

--- app/router.js
+++ app/router.js
@@ -1,9 +1,15 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
+import { inject as service } from '@ember/service';
 
 const Router = EmberRouter.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
+  headData: service(),
+
+  setTitle(title) {
+    this.get('headData').set('title', title);
+  }
 });
 
 Router.map(function() {
