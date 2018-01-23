5e5855c74d78e99eb6c9e2793fd8de290453d048
Brian Whitton <me@brianwhitton.com>
2018-01-23 16:41:53 -0500

    add a title to 404 page

--- app/routes/not-found.js
+++ app/routes/not-found.js
@@ -2,7 +2,10 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  title: 'Not Found',
+
   fastboot: service(),
+
   model() {
     if (this.get('fastboot.isFastBoot')) {
       this.set('fastboot.response.statusCode', 404);
