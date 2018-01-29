e19ca91233bbcd2a3fc42d0504685d3078645138
Brian Whitton <me@brianwhitton.com>
2018-01-29 12:54:00 -0500

    redirect from /thank-you if the submission `isNew`, i.e. unsaved

--- app/routes/survey/thank-you.js
+++ app/routes/survey/thank-you.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
+import { get } from '@ember/object';
 
 export default Route.extend({
+  redirect(model) {
+    if (get(model, 'submission.isNew')) {
+      this.transitionTo('survey.index');
+    }
+  }
 });
